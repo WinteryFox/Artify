f52c9919ffa27ffdff9ca84bec5c8b46340e8d33
Amy <>
2023-06-20 20:20:21 +0200

    fix sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,14 +8,13 @@ subprojects {
     version = project_version
     group = "com.artify"
 
-    apply(plugin = "org.sonarqube")
     apply(plugin = "com.google.cloud.tools.jib")
+}
 
-    sonarqube {
-        properties {
-            property("sonar.projectKey", "WinteryFox_Artify")
-            property("sonar.organization", "winteryfox")
-            property("sonar.host.url", "https://sonarcloud.io")
-        }
+sonarqube {
+    properties {
+        property("sonar.projectKey", "WinteryFox_Artify")
+        property("sonar.organization", "winteryfox")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
